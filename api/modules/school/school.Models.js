a04bb61a25a587c5eb710fc67c085d86eff9512e
Vadim <azzazzelor@gmail.com>
2019-07-14 17:01:52 +0300

    sec

--- api/modules/school/school.Models.js
+++ api/modules/school/school.Models.js
@@ -7,10 +7,10 @@ const schoolSchema = new Schema({
     id: Number,
     name : {
         type: String,
-        tutors : [{
-           type: Schema.Types.ObjectId, ref: 'Tutor'
-        }]
-    }
+    },
+    tutors : [{
+        type: Schema.Types.ObjectId, ref: 'Tutor'
+     }]
 })
 
 export default mongoose.model('School', schoolSchema);
